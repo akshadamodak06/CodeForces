b501a920e8fbb253da778d3cf92152a68c976e5c
akshadamodak06 <161637747+akshadamodak06@users.noreply.github.com>
2024-05-07 20:24:11 +0530

    Committing files

---
+++ CodeForces/C++20 (GCC 13-64)/1934A | Too Min Too Max/252974691.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int cases;
+    cin>>cases;
+    while(cases--)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++)
+        {
+            cin>>arr[i];
+        }
+        sort(arr,arr+n);
+        int ans=2*abs(arr[n-1]-arr[0])+2*abs(arr[n-2]-arr[1]);
+        cout<<ans<<endl;
+    }
+}
