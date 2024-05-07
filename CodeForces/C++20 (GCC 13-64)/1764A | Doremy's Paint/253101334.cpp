b3690cd75b973a1b650c64351e15ec5a28110d5f
akshadamodak06 <161637747+akshadamodak06@users.noreply.github.com>
2024-05-07 20:23:44 +0530

    Committing files

---
+++ CodeForces/C++20 (GCC 13-64)/1764A | Doremy's Paint/253101334.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int cases;
+    cin>>cases;
+    while(cases--)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++)
+        {
+            cin>>arr[n];
+        }
+        cout<<"1 "<<n<<endl;
+    }
+}
