965a5519ab26e496136eef81b7e6b40fc5423e03
akshadamodak06 <161637747+akshadamodak06@users.noreply.github.com>
2024-05-07 16:55:10 +0530

    Committing files

---
+++ CodeForces/C++20 (GCC 13-64)/1905A | Constructive Problems/253413122.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int cases;
+    cin>>cases;
+    while(cases--)
+    {
+        int n,m;
+        cin>>n>>m;
+        cout<<max(n,m)<<endl;
+    }
+}
+  
